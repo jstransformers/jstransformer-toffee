c34232814c37eb9993caac70cafa21aa95750e80
tunnckoCore <mameto_100@mail.bg>
2015-04-05 18:07:43 +0300

    move to `assertit`

--- test/index.js
+++ test/index.js
@@ -7,20 +7,19 @@
 
 'use strict';
 
-var test = require('testit');
-var assert = require('assert');
+var test = require('assertit');
 var transformer = require('../index');
 
 test('should compile a given template string', function(done) {
   var actual = transformer.compile('Hello #{place}!');
   var expected = 'function';
 
-  assert.strictEqual(typeof actual, expected);
+  test.equal(typeof actual, expected);
 
   actual = actual({place: 'world'});
   expected = 'Hello world!';
 
-  assert.strictEqual(actual, expected);
+  test.equal(actual, expected);
   done();
 });
 
@@ -28,7 +27,7 @@ test('should compile a given filepath with template (synchronously)', function(d
   var actual = transformer.compileFile('./test/fixture.toffee')({supplies: [1, 'b', 'c', 4]});
   var expected = '<li>1</li><li>b</li><li>c</li><li>4</li>';
 
-  assert.strictEqual(actual, expected);
+  test.equal(actual, expected);
   done();
 });
 
@@ -38,7 +37,7 @@ test('should compile a given filepath with template (async/promise)', function(d
 
   promise.then(function(compileFn) {
     var actual = compileFn({supplies: [1, 'b', 'c', 4]});
-    assert.strictEqual(actual, expected);
+    test.equal(actual, expected);
     done();
   })
 });
@@ -47,7 +46,7 @@ test('should render a given template string', function(done) {
   var actual = transformer.render('<li>#{supply}</li>', {supply: 'foo bar'});
   var expected = '<li>foo bar</li>';
 
-  assert.strictEqual(actual, expected);
+  test.equal(actual, expected);
   done();
 });
 
@@ -55,7 +54,7 @@ test('should render a given filepath with template (synchronously)', function(do
   var actual = transformer.renderFile('./test/fixture.toffee', {supplies: [1, 2, 3, 4]});
   var expected = '<li>1</li><li>2</li><li>3</li><li>4</li>';
 
-  assert.strictEqual(actual, expected);
+  test.equal(actual, expected);
   done();
 });
 
@@ -64,7 +63,7 @@ test('should render a given filepath with template (async/promise)', function(do
   var expected = '<li>1</li><li>2</li><li>3</li><li>4</li>';
 
   promise.then(function(actual) {
-    assert.strictEqual(actual, expected);
+    test.equal(actual, expected);
     done();
   });
 });
