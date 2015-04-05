254ac7da73d6e3234e709f1e955340f45cf6a4c2
tunnckoCore <mameto_100@mail.bg>
2015-04-06 00:16:09 +0300

    cleanup

--- test/index.js
+++ test/index.js
@@ -80,61 +80,16 @@ test('should render toffee file from a given filepath, synchronously', function(
   done();
 });
 
+// @todo something is wrong with `.renderFileAsync`
+// causing ReferenceError `supplies not defined`
 test('should render toffee file from a given filepath, async/promise', function(done) {
   var locals = {supplies: ['foo', 'bar', 'baz', 'qux']};
   var promise = transform.renderFileAsync('./test/fixture.toffee', {}, locals);
   var expected = '<li>foo</li><li>bar</li><li>baz</li><li>qux</li>';
 
   promise.then(function(actual) {
-    // console.log(actual) //=> '<p>Hello, my name is .</p>'
+    // console.log(actual)
     // test.equal(actual.body, expected);
     done();
   });
 });
-
-
-// test('should compile a given filepath with template (synchronously)', function(done) {
-//   var compiled = transform.compileFile('./test/fixture.toffee');
-//   var actual = compiled.fn({supplies: [1, 'b', 'c', 4]});
-//   var expected = '<li>1</li><li>b</li><li>c</li><li>4</li>';
-
-//   test.equal(actual, expected);
-//   done();
-// });
-
-// test('should compile a given filepath with template (async/promise)', function(done) {
-//   var promise = transform.compileFileAsync('./test/fixture.toffee');
-//   var expected = '<li>1</li><li>b</li><li>c</li><li>4</li>';
-
-//   promise.then(function(compiled) {
-//     var actual = compiled.fn({supplies: [1, 'b', 'c', 4]});
-//     test.equal(actual, expected);
-//     done();
-//   })
-// });
-
-// test('should render a given template string', function(done) {
-//   var actual = transform.render('<p>Hello #{place}</p>.', {}, {place: 'world'});
-//   var expected = '<p>Hello world!</p>';
-
-//   test.equal(actual, expected);
-//   done();
-// });
-
-// test('should render a given filepath with template (synchronously)', function(done) {
-//   var actual = transform.renderFile('./test/fixture.toffee', {}, {supplies: [1, 2, 3, 4]});
-//   var expected = '<li>1</li><li>2</li><li>3</li><li>4</li>';
-
-//   test.equal(actual, expected);
-//   done();
-// });
-
-// test('should render a given filepath with template (async/promise)', function(done) {
-//   var promise = transform.renderFileAsync('./test/fixture.toffee', {}, {supplies: [1, 2, 3, 4]});
-//   var expected = '<li>1</li><li>2</li><li>3</li><li>4</li>';
-
-//   promise.then(function(actual) {
-//     test.equal(actual, expected);
-//     done();
-//   });
-// });
